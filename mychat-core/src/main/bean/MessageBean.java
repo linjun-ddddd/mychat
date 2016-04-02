9186beb5ee2ff0e65fcae0c1697399dc53d50501
lj654548718 <654548718@qq.com>
2016-04-02 10:16:04 +0800

    Velocity+Spring Mvc

---
+++ mychat-core/src/main/bean/MessageBean.java
@@ -0,0 +1,64 @@
+package bean;
+
+import java.sql.SQLException;
+
+public class MessageBean {
+	String id;
+	String data;
+	String sendDate;
+	String type;
+	String toUserId;
+	String fromuserid;
+	String title;
+	String status;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getSendDate() {
+		return sendDate;
+	}
+	public void setSendDate(String sendDate) {
+		this.sendDate = sendDate;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getToUserId() {
+		return toUserId;
+	}
+	public void setToUserId(String toUserId) {
+		this.toUserId = toUserId;
+	}
+	public String getFromuserid() {
+		return fromuserid;
+	}
+	public void setFromuserid(String fromuserid) {
+		this.fromuserid = fromuserid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
---
+++ mychat-core/src/main/bean/UserBean.java
@@ -0,0 +1,31 @@
+ package bean;
+
+public class UserBean {
+	String id;
+	String name;
+	String password;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "用户详情..";
+	}
+	
+}
